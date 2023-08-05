147e10d7c291fb95852aca3a9435ade8323f8b1c
nischaytv <rocknischay@gmail.com>
2023-08-05 13:12:01 +0530

    changing test

--- cypress/e2e/headerTest.cy.js
+++ cypress/e2e/headerTest.cy.js
@@ -3,7 +3,7 @@
 describe('Header Test', () => {
   it('Verify Main Header', () => {
     cy.visit('https://nisu.azurewebsites.net/webapp/')
-    cy.get('h1').should('have.text','Nischay')
+    cy.get('h1').should('have.text','Raju')
   })
   it('Verify Second Header', () => {
     cy.visit('https://nisu.azurewebsites.net/webapp/')
